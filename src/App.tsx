8057be1a0e0d50e98f8711dfd4413ee0770ce43c
NatanWS <natanwsn@gmail.com>
2021-11-28 16:02:34 -0300

    feat/style: posts component created and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
 import Posts from "./components/Posts";
+import "./styles/app.scss";
 
 const App = () => {
   return (
-    <div>
+    <div className="App">
       <Header />
       <Posts />
     </div>
--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,24 +1,38 @@
 import React from "react";
 import { usePosts } from "../../providers/postsProvider";
 import Pagination from "../Pagination";
+import "../../styles/posts.scss";
+import Loading from "../Loading";
+import FailToLoad from "../FailToLoad";
 
 const Posts = () => {
   const { posts, error } = usePosts();
 
   return (
-    <div className="PostContainer">
-      {posts.length === 0 && error === false
-        ? "Loading..."
-        : error === true
-        ? "Failed to load"
-        : posts.map((post) => (
-            <div className="PostContainer__Post" key={post.id}>
-              <h3>{post.title}</h3>
-              <p>{post.body}</p>
+    <>
+      {posts.length === 0 && error === false ? (
+        <Loading />
+      ) : error === true ? (
+        <FailToLoad />
+      ) : (
+        <div className="Container">
+          <h2>Últimas postagens</h2>
+          <div className="PostContainer">
+            <div className="PostContainer__Header">
+              <span className="PostContainer__Title">Título</span>
+              <span className="PostContainer__Content">Conteúdo</span>
             </div>
-          ))}
-      <Pagination />
-    </div>
+            {posts.map((post) => (
+              <div className="PostContainer__Post" key={post.id}>
+                <span className="PostContainer__Title">{post.title}</span>
+                <span className="PostContainer__Content">{post.body}</span>
+              </div>
+            ))}
+          </div>
+          <Pagination />
+        </div>
+      )}
+    </>
   );
 };
 
